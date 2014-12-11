52cd9ad2eb1aa350c5227df9c2aaa61dc078d757
Dmitri Gaisan <dima.gaisan@gmail.com>
2014-12-11 17:24:31 -0600

    Adding Data to <App />

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -4,26 +4,51 @@
 var React = require('react');
 
 var ScrollItem = React.createClass({
+
     render: function() {
         return (
             <div>
                 <h2>{this.props.title}</h2>
-                <img src="{this.props.thumb}" />
+                <img src={this.props.thumb} />
             </div>
         );
     }
 });
 
 var App = React.createClass({
+
+    getInitialState: function() {
+        return {data: getData()};
+    },
+
     render: function() {
         return (
             <div>
-                {this.state.data.map(function(item) {
+                {this.state.data.map(function(item, i) {
                     return (
-                        <ScrollItem title={item.title} thumb={item.thumb}></ScrollItem>
+                        <ScrollItem title={item.title} thumb={item.thumb} key={i}></ScrollItem>
                     );
                 })}
             </div>
         );
     }
 });
+
+/**
+ * Random Data.
+ * @returns {{title: string, thumb: string}[]}
+ */
+function getData() {
+    var data = [
+        {title: 'Title #1', thumb: 'http://lorempixel.com/g/300/200/'},
+        {title: 'Title #2', thumb: 'http://lorempixel.com/300/200/'},
+        {title: 'Title #3', thumb: 'http://lorempixel.com/300/200/sports'},
+        {title: 'Title #4', thumb: 'http://lorempixel.com/g/300/200/people'},
+        {title: 'Title #5', thumb: 'http://lorempixel.com/300/200/fashion'},
+        {title: 'Title #6', thumb: 'http://lorempixel.com/g/300/200/cats'},
+    ];
+
+    return data;
+}
+
+module.exports = App;
